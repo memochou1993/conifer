83c820b537521a54c35ebab1858e29d484cf0aaf
Memo Chou <memochou1993@gmail.com>
2022-10-13 00:58:04 +0800

    Handle errors

--- src/handler.rs
+++ src/handler.rs
@@ -2,18 +2,30 @@ use crate::{
     repository,
     response::{RespGetRecord, RespGetRecords},
 };
-use rocket::serde::json::Json;
+use rocket::{http::Status, serde::json::Json};
 
 #[get("/records")]
-pub fn get_records() -> Json<RespGetRecords> {
+pub fn get_records() -> Result<Json<RespGetRecords>, Status> {
     let conn = &mut repository::connect();
     let records = repository::get_records(conn);
-    Json(RespGetRecords { data: records })
+    match records {
+        Ok(r) => match r {
+            Some(r) => Ok(Json(RespGetRecords { data: r })),
+            None => Err(Status::NotFound),
+        },
+        Err(_) => Err(Status::InternalServerError),
+    }
 }
 
 #[get("/records/<id>")]
-pub fn get_record(id: &str) -> Json<RespGetRecord> {
+pub fn get_record(id: &str) -> Result<Json<RespGetRecord>, Status> {
     let conn = &mut repository::connect();
     let record = repository::get_record(conn, id);
-    Json(RespGetRecord { data: record })
+    match record {
+        Ok(r) => match r {
+            Some(r) => Ok(Json(RespGetRecord { data: r })),
+            None => Err(Status::NotFound),
+        },
+        Err(_) => Err(Status::InternalServerError),
+    }
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,6 +1,8 @@
 use crate::model::Record;
+use crate::schema::records::dsl::{id, records};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::result::Error;
 use dotenvy::dotenv;
 use std::env;
 
@@ -11,18 +13,14 @@ pub fn connect() -> PgConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
-pub fn get_record(conn: &mut PgConnection, _id: &str) -> Record {
-    use crate::schema::records::dsl::{id, records};
+pub fn get_records(conn: &mut PgConnection) -> Result<Option<Vec<Record>>, Error> {
+    records.get_results::<Record>(conn).optional()
+}
+
+pub fn get_record(conn: &mut PgConnection, _id: &str) -> Result<Option<Record>, Error> {
     records
         .filter(id.eq(_id))
         .limit(1)
         .get_result::<Record>(conn)
-        .expect("Error loading record")
-}
-
-pub fn get_records(conn: &mut PgConnection) -> Vec<Record> {
-    use crate::schema::records::dsl::records;
-    records
-        .get_results::<Record>(conn)
-        .expect("Error loading records")
+        .optional()
 }
