c686cad93e9b47073e25e0b0ebd511ad1ea251bf
Memo Chou <memochou1993@gmail.com>
2022-10-22 00:44:59 +0800

    Handle CORS

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate rocket;
 use crate::handler::record;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
 
 mod database;
 mod handler;
@@ -8,6 +11,24 @@ mod model;
 mod request;
 mod response;
 mod schema;
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "CORS",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+    }
+}
 
 #[launch]
 pub fn rocket() -> _ {
@@ -17,4 +38,5 @@ pub fn rocket() -> _ {
         .mount("/api", routes![record::store])
         .mount("/api", routes![record::show])
         .mount("/api", routes![record::destroy])
+        .attach(CORS)
 }
