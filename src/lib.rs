d8002ce40ea3199b8a7ddd3777c3895924c6b0a3
Memo Chou <memochou1993@gmail.com>
2022-10-24 23:37:21 +0800

    Handle preflight requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,23 @@
 #[macro_use]
 extern crate rocket;
+use crate::fairing::cors::CORS;
 use crate::handler::record;
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
 
 mod database;
+mod fairing;
 mod handler;
 mod model;
 mod request;
 mod response;
 mod schema;
 
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "CORS",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-    }
-}
+#[options("/<_..>")]
+fn options() {}
 
 #[launch]
 pub fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![record::redirect])
+        .mount("/", routes![record::redirect, options])
         .mount("/api", routes![record::index])
         .mount("/api", routes![record::store])
         .mount("/api", routes![record::show])
